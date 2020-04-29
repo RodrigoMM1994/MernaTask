e7f5f7646f6659826b3dd7814cf51c88c6ac63d3
Rodrigo Mendoza <rodrigo.mendoza@tecnoaplicada.com>
2020-04-29 18:24:27 -0500

    ajustes para deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended : true}));
 
 // Puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 app.use('/api/usuarios',require('./routes/usuarios'));
@@ -23,13 +23,8 @@ app.use('/api/auth',require('./routes/auth'));
 app.use('/api/proyectos',require('./routes/proyecto'));
 app.use('/api/tareas',require('./routes/tareas'));
 
-// Definir la pagina pricipal
-app.get('/', (req,res) =>{
-    res.send("Hola Mundo");
-})
-
 // arrancar el servidor
 
-app.listen(PORT, () =>{
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port,'0.0.0.0', () =>{
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
